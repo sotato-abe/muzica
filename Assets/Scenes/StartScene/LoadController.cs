a4349a06a6cbe53dada552d58121d850a6f3eaa2
sotaro <s.abe@gmail.com>
2025-08-24 17:27:14 +0900

    faet : セーブデータボタンのオブジェクトを追加

---
+++ Assets/Scenes/StartScene/LoadController.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class LoadController : MonoBehaviour
+{
+    [SerializeField] GameObject loadDataList;
+    // [SerializeField] LoadDataButton loadDataButton;
+}
