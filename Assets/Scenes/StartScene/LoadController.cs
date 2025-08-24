006e83a017769fdd1ddf611ebb2da0283f092a5e
sotaro <s.abe@gmail.com>
2025-08-25 00:17:42 +0900

    feat : スタート画面でキャラクターデータからスタートする機能を追加

--- Assets/Scenes/StartScene/LoadController.cs
+++ Assets/Scenes/StartScene/LoadController.cs
@@ -9,11 +9,18 @@ public class LoadController : MonoBehaviour
     [SerializeField] LoadDataButton loadDataButton1;
     [SerializeField] LoadDataButton loadDataButton2;
     [SerializeField] LoadDataButton loadDataButton3;
+    [SerializeField] private PlayerCharacter sola;
+    [SerializeField] private PlayerCharacter huh;
 
-    public const string FILE_SOLA = "solaData.json";
     public const string FILE_NAME1 = "playData1.json";
     public const string FILE_NAME2 = "playData2.json";
     public const string FILE_NAME3 = "playData3.json";
+
+    private enum CharacterIndex
+    {
+        Sola,
+        Huh
+    }
 
     private void Start()
     {
@@ -50,11 +57,11 @@ public class LoadController : MonoBehaviour
     void StartGame()
     {
 
-        PlayData selectedPlayData = Persistance.Load<PlayData>(FILE_SOLA);
+        PlayData selectedPlayData = PlayerDataConverter(CharacterIndex.Sola);
         GameScene.selectedPlayData = selectedPlayData;
         SceneManager.LoadScene("GameScene");
     }
-    
+
     void LoadGame(int index)
     {
         PlayData selectedPlayData = index switch
@@ -66,5 +73,85 @@ public class LoadController : MonoBehaviour
         };
         GameScene.selectedPlayData = selectedPlayData;
         SceneManager.LoadScene("GameScene");
+    }
+
+    private PlayData PlayerDataConverter(CharacterIndex characterIndex)
+    {
+        PlayData playData = new PlayData();
+        PlayerCharacter character = characterIndex switch
+        {
+            CharacterIndex.Sola => sola,
+            CharacterIndex.Huh => huh,
+            _ => null
+        };
+        character.Init();
+
+        if (character == null) return playData;
+
+        int characterId = CharacterDatabase.Instance.GetCharacterId(character.Base);
+        PlayerData playerData = new PlayerData
+        {
+            characterId = characterId,
+            maxLife = character.MaxLife,
+            currentLife = character.Life,
+            maxBattery = character.MaxBattery,
+            currentBattery = character.Battery,
+            currentSoul = character.Soul,
+            power = character.Power,
+            technique = character.Technique,
+            defense = character.Defense,
+            speed = character.Speed,
+            luck = character.Luck,
+            memory = character.Memory,
+            storage = character.Storage,
+            pocket = character.Pocket,
+            coin = character.Coin,
+            disc = character.Disc,
+            key = character.Key,
+            level = character.Level,
+            skillPoint = character.SkillPoint,
+            exp = character.Exp,
+        };
+        if (character.RightHandEquipment != null)
+            playerData.rightEquipmentIndex = ItemDatabase.Instance.GetItemId(character.RightHandEquipment.Base);
+        if (character.LeftHandEquipment != null)
+            playerData.leftEquipmentIndex = ItemDatabase.Instance.GetItemId(character.LeftHandEquipment.Base);
+
+        foreach (var item in character.BagItemList)
+        {
+            int itemId = ItemDatabase.Instance.GetItemId(item.Base);
+            if (itemId != -1)
+                playerData.bagItemIndexList.Add(itemId);
+        }
+        foreach (var item in character.PocketList)
+        {
+            int itemId = ItemDatabase.Instance.GetItemId(item.Base);
+            if (itemId != -1)
+                playerData.pocketItemIndexList.Add(itemId);
+        }
+        foreach (var command in character.StorageList)
+        {
+            int commandId = CommandDatabase.Instance.GetCommandId(command.Base);
+            if (commandId != -1)
+                playerData.storageCommandIndexList.Add(commandId);
+        }
+        foreach (var command in character.SlotList)
+        {
+            if (command == null)
+            {
+                playerData.slotCommandIndexList.Add(-1);
+                continue;
+            }
+            int commandId = CommandDatabase.Instance.GetCommandId(command.Base);
+            if (commandId != -1)
+                playerData.slotCommandIndexList.Add(commandId);
+            else
+                playerData.slotCommandIndexList.Add(-1);
+        }
+        playData.playerData = playerData;
+        playData.position = character.Base.Birthplace.Position;
+        playData.time = new DateTime(2030, 12, 1); 
+
+        return playData;
     }
 }
