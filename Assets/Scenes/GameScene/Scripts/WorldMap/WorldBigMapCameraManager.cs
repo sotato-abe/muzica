315841fb5f72400dd706bfbc83475a418ab7f5bc
sotaro <s.abe@gmail.com>
2025-08-29 00:40:30 +0900

    feat : ワールドマップを追加

---
+++ Assets/Scenes/GameScene/Scripts/WorldMap/WorldBigMapCameraManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldBigMapCameraManager : MonoBehaviour
+{
+    private int worldHeight = 100; // ワールドマップの高さ
+
+    private Vector3Int currentPos; // 現在のカメラ位置
+
+    // WorldMapのPlayer位置にカメラを合わせる
+    public void TargetPlayer(Vector3Int targetPos)
+    {
+        currentPos = targetPos; // 現在のカメラ位置を更新
+        // カメラの位置をターゲットの位置に合わせる
+        Vector3 cameraPos = new Vector3(20, targetPos.y, -10); // Z軸は-10に固定
+        transform.position = cameraPos;
+    }
+
+    public void ResetCamera()
+    {
+        TargetPlayer(currentPos); // 現在のカメラ位置に戻す
+    }
+
+    public void UpTarget()
+    {
+        // カメラの位置を上に移動
+        Vector3 cameraPos = transform.position;
+        cameraPos.y += 0.05f; // 上に1ユニット移動
+        if (cameraPos.y > worldHeight) // 上限を超えないように制限
+        {
+            cameraPos.y = 0;
+        }
+        transform.position = cameraPos;
+    }
+
+    public void DownTarget()
+    {
+        // カメラの位置を下に移動
+        Vector3 cameraPos = transform.position;
+        cameraPos.y -= 0.05f; // 下に1ユニット移動
+        if (cameraPos.y < 0) // 下限を超えないように制限
+        {
+            cameraPos.y = worldHeight;
+        }
+        transform.position = cameraPos;
+    }
+
+    public void RightTarget()
+    {
+        // カメラの位置を右に移動
+        Vector3 cameraPos = transform.position;
+        cameraPos.x += 0.05f; // 右に1ユニット移動
+        transform.position = cameraPos;
+    }
+
+    public void LeftTarget()
+    {
+        // カメラの位置を左に移動
+        Vector3 cameraPos = transform.position;
+        cameraPos.x -= 0.05f; // 左に1ユニット移動
+        transform.position = cameraPos;
+    }
+}
---
+++ Assets/Scenes/GameScene/Scripts/WorldMap/WorldBigMapPanel.cs
@@ -0,0 +1,106 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class WorldBigMapPanel : Panel
+{
+    [SerializeField] WorldBigMapCameraManager worldMapCameraManager;
+    [SerializeField] AgeTimePanel ageTimePanel;
+    [SerializeField] FieldPlayer fieldPlayer;
+
+    private bool isCameraUpFlg = false;
+    private bool isCameraBottomFlg = false;
+    private bool isCameraRightFlg = false;
+    private bool isCameraLeftFlg = false;
+
+    void Update()
+    {
+        if (!isActive) return; // フラグがfalseの場合は処理をスキップ
+
+        if (isCameraUpFlg || Input.GetKey(KeyCode.UpArrow))
+        {
+            worldMapCameraManager.UpTarget(); // 上に移動
+        }
+        if (isCameraBottomFlg || Input.GetKey(KeyCode.DownArrow))
+        {
+            worldMapCameraManager.DownTarget(); // 下に移動
+        }
+        if (isCameraRightFlg || Input.GetKey(KeyCode.RightArrow))
+        {
+            worldMapCameraManager.RightTarget(); // 右に移動
+        }
+        if (isCameraLeftFlg || Input.GetKey(KeyCode.LeftArrow))
+        {
+            worldMapCameraManager.LeftTarget(); // 左に移動
+        }
+        if (Input.inputString.Contains("@"))
+        {
+            OnCurrentPosition(); // 下に移動
+        }
+    }
+
+    public void SwitchActive()
+    {
+        isActive = !isActive; // フラグをトグル
+        if (isActive)
+        {
+            ageTimePanel.SetTimeSpeed(TimeState.Live); // 時間を止める
+            PanelOpen();
+        }
+        else
+        {
+            ageTimePanel.SetTimeSpeed(TimeState.Fast); // 時間を進める
+            ClosePanel();
+        }
+        fieldPlayer.SetCanMove(!isActive);
+    }
+
+    public void OnUpStart()
+    {
+        Debug.Log("Up button pressed");
+        isCameraUpFlg = true;
+    }
+
+    public void OnUpEnd()
+    {
+        isCameraUpFlg = false;
+    }
+
+    public void OnDownStart()
+    {
+        isCameraBottomFlg = true;
+    }
+    public void OnDownEnd()
+    {
+        isCameraBottomFlg = false;
+    }
+
+    public void OnRightStart()
+    {
+        Debug.Log("Right button pressed");
+        isCameraRightFlg = true;
+    }
+
+    public void OnRightEnd()
+    {
+        isCameraRightFlg = false;
+    }
+
+    public void OnLeftStart()
+    {
+        Debug.Log("Left button pressed");
+        isCameraLeftFlg = true;
+    }
+
+    public void OnLeftEnd()
+    {
+        isCameraLeftFlg = false;
+    }
+
+    public void OnCurrentPosition()
+    {
+        worldMapCameraManager.ResetCamera(); // カメラの位置をリセット
+    }
+}
