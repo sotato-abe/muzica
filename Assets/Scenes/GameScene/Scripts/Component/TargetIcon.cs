08b2546812dff64d9bcba243ca17d91e85900421
sotaro <s.abe@gmail.com>
2025-09-07 01:59:20 +0900

    feat : 装備Infoのターゲットを全対応

--- Assets/Scenes/GameScene/Scripts/Component/TargetIcon.cs
+++ Assets/Scenes/GameScene/Scripts/Component/TargetIcon.cs
@@ -6,22 +6,30 @@ using UnityEngine.UI;
 public class TargetIcon : MonoBehaviour
 {
     [SerializeField] Image iconImage;
+    [SerializeField] Sprite selfImage;
     [SerializeField] Sprite individualImage;
-    [SerializeField] Sprite grouplImage;
+    [SerializeField] Sprite groupImage;
     [SerializeField] Sprite allImage;
+    [SerializeField] Sprite randomImage;
 
     public void SetTargetType(TargetType targetType)
     {
         switch (targetType)
         {
+            case TargetType.Self:
+                iconImage.sprite = selfImage;
+                break;
             case TargetType.Individual:
                 iconImage.sprite = individualImage;
                 break;
             case TargetType.Group:
-                iconImage.sprite = grouplImage;
+                iconImage.sprite = groupImage;
                 break;
             case TargetType.All:
                 iconImage.sprite = allImage;
+                break;
+            case TargetType.Random:
+                iconImage.sprite = randomImage;
                 break;
             default:
                 Debug.LogWarning("Unknown TargetType: " + targetType);
