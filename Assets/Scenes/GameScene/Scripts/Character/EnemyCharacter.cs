47f22edf711683a4afe35dbc015f7ab123b6535c
sotaro <s.abe@gmail.com>
2025-09-08 12:07:12 +0900

    feat : エネミーのレベルアップ時のステータス変更に重みを追加

--- Assets/Scenes/GameScene/Scripts/Character/EnemyCharacter.cs
+++ Assets/Scenes/GameScene/Scripts/Character/EnemyCharacter.cs
@@ -3,20 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
+using System.Linq;
 
 [System.Serializable]
 public class EnemyCharacter : Character
 {
     public EnemyCharacter(CharacterBase baseData) : base(baseData) { }
+    private Dictionary<StatusType, int> statusDictionary = new Dictionary<StatusType, int>();
 
     public void LevelUp(int increase)
     {
+        statusDictionary = new Dictionary<StatusType, int>();
+
+        statusDictionary[StatusType.LIFE] = MaxLife / 10;
+        statusDictionary[StatusType.BTRY] = MaxBattery / 5;
+        statusDictionary[StatusType.POW] = Power;
+        statusDictionary[StatusType.DEF] = Defense;
+        statusDictionary[StatusType.TEC] = Technique;
+        statusDictionary[StatusType.SPD] = Speed;
+        statusDictionary[StatusType.LUK] = Luck;
+
         for (int i = 0; i < increase; i++)
         {
-            StatusType randomStatus = (StatusType)Random.Range(0, 6);
-            Debug.Log($"{randomStatus}");
+            StatusType targetStatus = WeightedRandomChoice();
 
-            switch (randomStatus)
+            switch (targetStatus)
             {
                 case StatusType.LIFE:
                     MaxLife += 10;
@@ -45,6 +56,31 @@ public class EnemyCharacter : Character
         }
         Level = increase;
     }
+
+    private StatusType WeightedRandomChoice()
+    {
+        // ステータスが高いものほど選ばれやすいように確率を設定
+        // １番: 40%, ２番: 25%, ３番: 15%, ４番: 10%, ５番: 6%, ６番: 4%
+        float[] probabilities = { 0.4f, 0.25f, 0.15f, 0.10f, 0.06f, 0.04f };
+
+        float r = Random.value;
+        float cumulative = 0f;
+
+        int index = 0;
+        foreach (var key in statusDictionary.Keys)
+        {
+            if (index >= probabilities.Length) break; // 安全対策
+            cumulative += probabilities[index];
+            if (r < cumulative)
+            {
+                return key;
+            }
+            index++;
+        }
+
+        return statusDictionary.Keys.First();
+    }
+
 
     public TotalAttackCount EnemyAttack()
     {
