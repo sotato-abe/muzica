042aa3624d3aa168957d5b183072299ff01fc6c0
sotaro <s.abe@gmail.com>
2025-09-08 23:25:55 +0900

    style : リザーブパネルとトレードパネルのスタイル変更

--- Assets/Scenes/GameScene/Scripts/Item/ItemType.cs
+++ Assets/Scenes/GameScene/Scripts/Item/ItemType.cs
@@ -13,7 +13,7 @@ public static class ItemTypeExtensions
     {
         return itemType switch
         {
-            ItemType.Consumable => new Color(180 / 255f, 219 / 255f, 61 / 255f),
+            ItemType.Consumable => new Color(61 / 255f, 167 / 255f, 219 / 255f),
             ItemType.Equipment => new Color(80 / 255f, 80 / 255f, 80 / 255f),
             ItemType.Treasure => new Color(219 / 255f, 166 / 255f, 61 / 255f),
             _ => Color.clear
