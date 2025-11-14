6978c3bd093432e1db5f4fc2a2f823a9dd548b9f
sotaro <s.abe@gmail.com>
2025-11-14 23:05:31 +0900

    feat : ゲームシーンにSettingWindowを実装

--- Assets/Scenes/GameScene/Scripts/Option/OptionGroup.cs
+++ Assets/Scenes/GameScene/Scripts/Option/OptionGroup.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class OptionGroup : MonoBehaviour
+public class OptionSystem : MonoBehaviour
 {
+    [SerializeField] Image backPanel;
+
     [SerializeField] WorldBigMapPanel worldBigMapPanel;
     [SerializeField] LibraryPanel libraryPanel;
     [SerializeField] SavePanel savePanel;
-    [SerializeField] ConfigPanel configPanel;
+    [SerializeField] SettingWindow settingWindow;
     [SerializeField] AgeTimePanel ageTimePanel;
+
+    private Color blockColor = new Color(0f, 0f, 0f, 200f / 255f);
+    private Color inBlockColor = new Color(0f, 0f, 0f, 0f);
 
     private List<Panel> panelList;
 
@@ -20,7 +25,7 @@ public class OptionGroup : MonoBehaviour
             worldBigMapPanel,
             libraryPanel,
             savePanel,
-            configPanel
+            settingWindow
         };
     }
 
@@ -33,12 +38,41 @@ public class OptionGroup : MonoBehaviour
                 if (panel.isActive)
                 {
                     panel.ClosePanel();
+                    CloseWindow();
                     PlayerController.Instance.SetFieldPlayerMove(true);
                     StopTimeState(false);
                     break;
                 }
             }
         }
+    }
+    public void OpenWindow()
+    {
+        SoundSystem.Instance.PlaySE(SeType.PanelOpen); // SEをオープンに変更
+        StartCoroutine(BlockChange(true));
+    }
+
+    public void CloseWindow()
+    {
+        SoundSystem.Instance.PlaySE(SeType.PanelClose);
+        StartCoroutine(BlockChange(false));
+    }
+
+    private IEnumerator BlockChange(bool isOpen)
+    {
+        float duration = 0.2f;
+        float elapsedTime = 0f;
+        Color startColor = backPanel.color;
+        Color targetColor = isOpen ? blockColor : inBlockColor;
+        while (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = Mathf.Clamp01(elapsedTime / duration);
+            backPanel.color = Color.Lerp(startColor, targetColor, t);
+            yield return null;
+        }
+        backPanel.color = targetColor;
+        backPanel.raycastTarget = isOpen;
     }
 
     public void SwitchWorldBigMapPanel()
@@ -56,24 +90,27 @@ public class OptionGroup : MonoBehaviour
         SwitchPanel(savePanel);
     }
 
-    public void SwitchConfigPanel()
+    public void SwitchSettingWindow()
     {
-        SwitchPanel(configPanel);
+        SwitchPanel(settingWindow);
     }
 
     private void SwitchPanel(Panel panel)
     {
+        SoundSystem.Instance.PlaySE(SeType.Select);
         foreach (var p in panelList)
         {
             if (p == panel && !p.isActive)
             {
                 p.PanelOpen();
+                OpenWindow();
                 PlayerController.Instance.SetFieldPlayerMove(false);
                 StopTimeState(true);
             }
             else if (p == panel && p.isActive)
             {
                 p.ClosePanel();
+                CloseWindow();
                 PlayerController.Instance.SetFieldPlayerMove(true);
                 StopTimeState(false);
             }
