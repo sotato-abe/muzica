ba36400aa34d11511c4248b4866245a1b93229b3
sotaro <s.abe@gmail.com>
2025-11-29 16:39:02 +0900

    feat : アイテムカードにフレームを追加

--- Assets/Scenes/GameScene/Scripts/Component/Card.cs
+++ Assets/Scenes/GameScene/Scripts/Component/Card.cs
@@ -8,6 +8,7 @@ public class Card : MonoBehaviour
 {
     [SerializeField] CardTitle cardTitle;
     [SerializeField] Image cardTypeIcon;
+    [SerializeField] Image cardFrame;
     [SerializeField] Image cardImage;
 
     public virtual void SetCard(Item item)
@@ -15,6 +16,7 @@ public class Card : MonoBehaviour
         this.gameObject.SetActive(true);
         cardImage.sprite = item.Base.Sprite;
         cardTypeIcon.color = item.Base.Rarity.GetRarityColor();
+        cardFrame.color = item.Base.Rarity.GetRarityColor();
         cardTitle.SetCardTitle(item.Base.Rarity, item.Base.Name);
     }
 
@@ -23,6 +25,7 @@ public class Card : MonoBehaviour
         this.gameObject.SetActive(true);
         cardImage.sprite = command.Base.Sprite;
         cardTypeIcon.color = command.Base.Rarity.GetRarityColor();
+        cardFrame.color = command.Base.Rarity.GetRarityColor();
         cardTitle.SetCardTitle(command.Base.Rarity, command.Base.Name);
     }
 }
