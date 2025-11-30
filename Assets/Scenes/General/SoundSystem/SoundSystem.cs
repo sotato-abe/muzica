5e50acfae1d2668e3d08e97a38d5773574a8b83e
sotaro <s.abe@gmail.com>
2025-11-30 19:15:25 +0900

    fix：ボリューム設定をシーンを引き継ぐように修正

--- Assets/Scenes/General/SoundSystem/SoundSystem.cs
+++ Assets/Scenes/General/SoundSystem/SoundSystem.cs
@@ -6,6 +6,12 @@ public class SoundSystem : MonoBehaviour
 {
     public static SoundSystem Instance { get; private set; }
 
+    // PlayerPrefsキー定数
+    private const string MASTER_VOLUME_KEY = "MasterVolume";
+    private const string BGM_VOLUME_KEY = "BGMVolume";
+    private const string SE_VOLUME_KEY = "SEVolume";
+    private const string AMBIENT_VOLUME_KEY = "AmbientVolume";
+
     [SerializeField] private AudioSource bgmSource;
     [SerializeField] private AudioSource seSource;
     [SerializeField] private AudioSource ambientSource;
@@ -25,16 +31,43 @@ public class SoundSystem : MonoBehaviour
 
     private void Awake()
     {
+        UnityEngine.Debug.Log("SoundSystem Awake");
         if (Instance == null)
         {
             Instance = this;
             DontDestroyOnLoad(gameObject); // シーン切り替えても残す
-
+            
+            // 保存されたボリューム設定を読み込み
+            LoadVolumeSettings();
         }
         else
         {
             Destroy(gameObject); // 重複防止
         }
+    }
+
+    private void Start()
+    {
+        // シーン切り替え後にAudioSourceが再アタッチされた場合の対応
+        ApplyVolumeSettings();
+    }
+
+    private void LoadVolumeSettings()
+    {
+        masterVolume = PlayerPrefs.GetFloat(MASTER_VOLUME_KEY, 1.0f);
+        bgmVolume = PlayerPrefs.GetFloat(BGM_VOLUME_KEY, 1.0f);
+        seVolume = PlayerPrefs.GetFloat(SE_VOLUME_KEY, 1.0f);
+        ambientVolume = PlayerPrefs.GetFloat(AMBIENT_VOLUME_KEY, 0.7f);
+        
+        // AudioSourceにボリュームを適用
+        ApplyVolumeSettings();
+    }
+
+    private void ApplyVolumeSettings()
+    {
+        if (bgmSource != null) bgmSource.volume = bgmVolume * masterVolume;
+        if (seSource != null) seSource.volume = seVolume * masterVolume;
+        if (ambientSource != null) ambientSource.volume = ambientVolume * masterVolume;
     }
 
     #region BGM
@@ -90,12 +123,13 @@ public class SoundSystem : MonoBehaviour
         bgmSource.volume = 0;
         bgmSource.Play();
 
+        float targetVolume = bgmVolume * masterVolume;
         for (float t = 0; t < fadeInTime; t += Time.deltaTime)
         {
-            bgmSource.volume = Mathf.Lerp(0, 1, t / fadeInTime);
+            bgmSource.volume = Mathf.Lerp(0, targetVolume, t / fadeInTime);
             yield return null;
         }
-        bgmSource.volume = 1;
+        bgmSource.volume = targetVolume;
     }
 
     public void StopBGM()
@@ -138,6 +172,7 @@ public class SoundSystem : MonoBehaviour
         if (ambientData != null)
         {
             ambientSource.clip = ambientData.AmbientClip();
+            ambientSource.volume = ambientVolume * masterVolume;
             ambientSource.loop = true;
             ambientSource.Play();
         }
@@ -159,12 +194,13 @@ public class SoundSystem : MonoBehaviour
         ambientSource.volume = 0;
         ambientSource.Play();
 
+        float targetVolume = ambientVolume * masterVolume;
         for (float t = 0; t < fadeInTime; t += Time.deltaTime)
         {
-            ambientSource.volume = Mathf.Lerp(0, 1, t / fadeInTime);
+            ambientSource.volume = Mathf.Lerp(0, targetVolume, t / fadeInTime);
             yield return null;
         }
-        ambientSource.volume = 1;
+        ambientSource.volume = targetVolume;
     }
 
     public void StopAmbient()
@@ -207,6 +243,11 @@ public class SoundSystem : MonoBehaviour
         masterVolume = volume;
         bgmSource.volume = bgmVolume * masterVolume;
         seSource.volume = seVolume * masterVolume;
+        ambientSource.volume = ambientVolume * masterVolume;
+        
+        // PlayerPrefsに保存
+        PlayerPrefs.SetFloat(MASTER_VOLUME_KEY, masterVolume);
+        PlayerPrefs.Save();
     }
 
     public float GetMasterVolume()
@@ -216,12 +257,18 @@ public class SoundSystem : MonoBehaviour
 
     public void SetBGMVolume(float volume)
     {
+        UnityEngine.Debug.Log("SetBGMVolume: " + volume);
         bgmVolume = volume;
         bgmSource.volume = bgmVolume * masterVolume;
+        
+        // PlayerPrefsに保存
+        PlayerPrefs.SetFloat(BGM_VOLUME_KEY, bgmVolume);
+        PlayerPrefs.Save();
     }
 
     public float GetBGMVolume()
     {
+        UnityEngine.Debug.Log("GetBGMVolume: " + bgmVolume);
         return bgmVolume;
     }
 
@@ -229,6 +276,10 @@ public class SoundSystem : MonoBehaviour
     {
         seVolume = volume;
         seSource.volume = seVolume * masterVolume;
+        
+        // PlayerPrefsに保存
+        PlayerPrefs.SetFloat(SE_VOLUME_KEY, seVolume);
+        PlayerPrefs.Save();
     }
 
     public float GetSEVolume()
@@ -240,6 +291,10 @@ public class SoundSystem : MonoBehaviour
     {
         ambientVolume = volume;
         ambientSource.volume = ambientVolume * masterVolume;
+        
+        // PlayerPrefsに保存
+        PlayerPrefs.SetFloat(AMBIENT_VOLUME_KEY, ambientVolume);
+        PlayerPrefs.Save();
     }
     public float GetAmbientVolume()
     {
