8e138e6ef5952457f044fd5d9c64140a1cf9ef7c
sotaro <s.abe@gmail.com>
2025-11-30 18:41:22 +0900

    feat : 設定で環境音のボリュームを変更できるように修正

--- Assets/Scenes/General/SettingSystem/SettingWindow.cs
+++ Assets/Scenes/General/SettingSystem/SettingWindow.cs
@@ -6,12 +6,14 @@ public class SettingWindow : Window
     [SerializeField] Slider masterSlider;
     [SerializeField] Slider bgmSlider;
     [SerializeField] Slider seSlider;
+    [SerializeField] Slider ambientSlider;
 
     private void Start()
     {
         masterSlider.value = SoundSystem.Instance.GetMasterVolume();
         bgmSlider.value = SoundSystem.Instance.GetBGMVolume();
         seSlider.value = SoundSystem.Instance.GetSEVolume();
+        ambientSlider.value = SoundSystem.Instance.GetAmbientVolume();
     }
 
     public void OnChangeMasterVolume(float volume)
@@ -27,5 +29,10 @@ public class SettingWindow : Window
     public void OnChangeSEVolume(float volume)
     {
         SoundSystem.Instance.SetSEVolume(volume);
+    }
+    
+    public void OnChangeAmbientVolume(float volume)
+    {
+        SoundSystem.Instance.SetAmbientVolume(volume);
     }
 }
